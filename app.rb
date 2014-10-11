3f0583508e8e1f8fe261368d7360cac923c0dbe7
Tim Green <tiimgreen@gmail.com>
2014-10-11 01:28:01 +0100

    Add playback feature

--- app.rb
+++ app.rb
@@ -1,9 +1,49 @@
 require 'toc'
 require 'taglib'
+require 'audite'
+require 'curses'
 
 def change_option(key, val)
   puts "\nCurrent #{key}:".bold.black.underline + " " + val.to_s.bold.red
   puts "Type the new #{key} or press Enter to leave as it is:"
+end
+
+def play_song(path)
+  player = Audite.new
+
+  player.events.on(:position_change) do |pos|
+    p = (player.tell / player.length * Curses.cols).ceil
+    l = (player.level * Curses.cols).ceil
+    minutes = (player.position.ceil / 60).floor
+    total_minutes = (player.length_in_seconds.ceil / 60).floor
+    Curses.setpos(0, 0)
+    Curses.addstr("#{minutes}:#{player.position.ceil - (minutes * 60)} // #{total_minutes}:#{player.length_in_seconds.ceil - (total_minutes * 60)}")
+    Curses.setpos(1, 0)
+    Curses.addstr("#" * p + " " * (Curses.cols - p))
+    Curses.setpos(2, 0)
+    Curses.addstr(">" * l + " " * (Curses.cols - l))
+    Curses.refresh
+  end
+
+  player.events.on(:complete) do
+    player.close if !player.active?
+  end
+
+  player.load(path)
+  player.start_stream
+
+  while c = Curses.getch
+    case c
+    when Curses::KEY_LEFT
+      player.rewind
+    when Curses::KEY_RIGHT
+      player.forward
+    when ' '
+      player.toggle
+    end
+  end
+
+  player.thread.join
 end
 
 puts "Drag and drop the file into this window: "
@@ -37,4 +77,11 @@ TagLib::MPEG::File.open(filepath) do |file|
 
   file.save
   puts "File saved!".green.bold
+
+  puts "Do you want to play the song?"
+  puts " 1) Yes"
+  puts " 2) No"
+  play_song = gets.chomp
+
+  play_song(filepath) if play_song == '1'
 end
